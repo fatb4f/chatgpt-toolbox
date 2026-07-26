907fc510c81316c68e8bad31d0c824f7c6598e5e
_404 <_404@example.com>
2026-07-26 00:41:40 -0400

    chore

--- src/toolbox/tools/go_git.py
+++ src/toolbox/tools/go_git.py
@@ -2,12 +2,12 @@ from toolbox.model import AcquisitionKind, AcquisitionSpec, ToolRole, ToolSpec
 
 SPEC = ToolSpec(
     name="go-git",
-    version="unresolved",
+    version="v5.19.1",
     target="x86_64-unknown-linux-gnu",
     acquisition=AcquisitionSpec(
         kind=AcquisitionKind.GO_MODULE,
         module="github.com/go-git/go-git/v5",
-        version=None,
+        version="v5.19.1",
     ),
     roles=frozenset({ToolRole.MODULE}),
 )
